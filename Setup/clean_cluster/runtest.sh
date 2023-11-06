f60b33015f2299284583575219d488b7dbc9a026
Sergio Arroutbi <sarroutb@redhat.com>
2023-11-06 10:22:55 +0100

    First basic test case (packit CI included) (#2)
    Resolves: #1
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

---
+++ Setup/clean_cluster/runtest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   runtest.sh of /CoreOS/attestation-operator/Setup/clean_cluster
+#   Description: Basic functionality tests of the tang operator
+#   Author: Sergio Arroutbi <sarroutb@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Copyright (c) 2021 Red Hat, Inc.
+#
+#   This program is free software: you can redistribute it and/or
+#   modify it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 2 of
+#   the License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE.  See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see http://www.gnu.org/licenses/.
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Include Beaker environment
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartCleanup
+        rlRun ". ../../TestHelpers/functions.sh" || rlDie "cannot import function script"
+        rlRun "checkClusterStatus" 0 "Checking cluster status"
+        if [ "${DISABLE_BUNDLE_INSTALL_TESTS}" != "1" ] && [ "${DISABLE_BUNDLE_UNINSTALL_TESTS}" != "1" ];
+        then
+          rlRun "cleanHelmDistro" 0 "Cleaning installed attestation-operator"
+        fi
+    rlPhaseEnd
+rlJournalEnd
