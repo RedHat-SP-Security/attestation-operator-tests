10da1cab140e87e75e2c43e5cbcda68df203f07f
Sergio Arroutbi <sarroutb@redhat.com>
2023-11-16 14:30:23 +0100

    Remove bundle checks (helm checks will be used) (#23)
    Resolves: #22
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- Setup/clean_cluster/runtest.sh
+++ Setup/clean_cluster/runtest.sh
@@ -33,9 +33,6 @@ rlJournalStart
     rlPhaseStartCleanup
         rlRun 'rlImport "common-cloud-orchestration/TestHelpers"' || rlDie "cannot import function script"
         rlRun "ocpopCheckClusterStatus" 0 "Checking cluster status"
-        if [ "${DISABLE_BUNDLE_INSTALL_TESTS}" != "1" ] && [ "${DISABLE_BUNDLE_UNINSTALL_TESTS}" != "1" ];
-        then
-          rlRun "ocpopCleanHelmDistro" 0 "Cleaning installed attestation-operator"
-        fi
+        rlRun "ocpopCleanHelmDistro" 0 "Cleaning installed attestation-operator"
     rlPhaseEnd
 rlJournalEnd
